ce32f790d0ae719043fe70e211156f11e855d67d
Juan Rikelme <128755900+RikelmeJ@users.noreply.github.com>
2024-08-26 01:14:08 -0300

    Add files via upload

---
+++ atividade.py
@@ -0,0 +1,16 @@
+#definir os valores
+int = 10
+float = 3.14
+boolean = True
+string = "aula de Clebão!"
+
+
+array = [1, 2, 3, 4, 5]  # Array com 5 posições
+
+
+# Exibir os valores armazenados
+print("Int:", int)
+print("float:", float)
+print("Booleano:", boolean)
+print("String:", string)
+print("Array:", array)
